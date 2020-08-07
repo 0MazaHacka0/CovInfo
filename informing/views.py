bf0167d29cdc1851bc28cbbe5e24af3582124a71
Dmitry Abakumov <killerinshadow2@gmail.com>
2020-08-07 21:03:22 +0300

    Add home page

--- informing/views.py
+++ informing/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from . import forms
 
 
+# Home
+def home(request):
+    return render(request, 'index.html', {})
+
+
 # User
 def signup(request):
     form = forms.PatientSignupForm(request.POST or None)
@@ -10,17 +15,3 @@ def signup(request):
         patient = form.save()
 
     return render(request, 'signup.html', {'form': form})
-
-    #
-    # if request.method == 'POST':
-    #     form = UserCreationForm(request.POST or None)
-    #     if form.is_valid():
-    #         form.save()
-    #     username = form.cleaned_data.get('username')
-    #     my_password = form.cleaned_data.get('password1')
-    #     user = authenticate(username=username, password=my_password)
-    #     login(request, user)
-    #     return redirect('index')
-    # else:
-    #     form = UserCreationForm()
-    #     return render(request, 'signup.html', {'form': form})
