62b1d627c2a7397332fb65c5822cf9ba1a566347
nik1998 <beregnovnikita@gmail.com>
2020-08-08 00:48:43 +0300

    add vaccination
    [re #3]

--- informing/urls.py
+++ informing/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('signup', views.signup, name='signup'),
+    path('vaccination', views.vaccination, name='vaccination')
 ]
